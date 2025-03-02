0d2ea1607d8752518d928f6fedc93c5fc9c720cf
Mika Stiebitz <mika.stiebitz@gmail.com>
2025-03-02 21:09:52 +0100

    Add local example

---
+++ example/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "../src/**/*.{js,jsx,ts,tsx}", // Include your library components
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
